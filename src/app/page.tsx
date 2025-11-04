5cc63c074967de6b8834b0362df134e8406d99bb
Ahhhhhyeong <dkdud081@gmail.com>
2025-11-04 11:04:27 +0900

    [fix] 뭔가 고침

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function OnboardingPage() {
   return (
-    <div className='w-[392px] h-screen flex flex-col max-w-full'>
+    <div className='h-screen flex flex-col max-w-full'>
       {/* 온보딩 콘텐츠 */}
-      <div className='flex-1 flex flex-col justify-center items-center p-6'>
+      <div className='flex-1 flex flex-col justify-center items-center text-center p-6'>
         <h1 className='text-2xl font-bold mb-4'>환영합니다!</h1>
         <p className='text-gray-600 text-center mb-8'>앱을 시작하기 위해 몇 가지 설정을 해보세요.</p>
         {/* 온보딩 단계들 */}
