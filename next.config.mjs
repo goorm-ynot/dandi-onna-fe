ccea6e4b8e4be349efa9eb9ca09245d77e0d8b37
Ahhhhhyeong <dkdud081@gmail.com>
2025-11-25 09:06:15 +0900

    [fix] build error

--- next.config.mjs
+++ next.config.mjs
@@ -33,12 +33,12 @@ const nextConfig = {
   },
   // kakao map 설정 추가
   experimental: {
-    optimizeCss: true, // CSS 최적화
     esmExternals: 'loose',
     // SWC에서 browserslist 사용
     browsersListForSwc: true,
     // 최신 브라우저 타겟팅
     legacyBrowsers: false,
+    // optimizeCss: true, // CSS 최적화
   },
   webpack: (config, { dev, isServer }) => {
     // 카카오 맵 SDK를 위한 웹팩 설정
