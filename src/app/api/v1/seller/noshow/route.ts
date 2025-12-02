82ad507c029b9eb38215033b8c6d7e76957aeb79
Ahhhhhhyeong <dkdud081@gmail.com>
2025-12-02 11:03:33 +0900

    update

--- src/app/api/v1/seller/noshow/route.ts
+++ src/app/api/v1/seller/noshow/route.ts
@@ -14,12 +14,22 @@ export async function GET(request: NextRequest) {
 
     // console.log('Query Params:', { date, page, size });
 
+    // date가 yyyy-MM-dd 형식으로 맵핑, 2025-01-01 형태
+    let formattedDate = '';
+    if (date) {
+      const dateObj = new Date(date);
+      const year = dateObj.getFullYear();
+      const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+      const day = String(dateObj.getDate()).padStart(2, '0');
+      formattedDate = `${year}-${month}-${day}`;
+    }
+
     // ✅ await 추가 및 백엔드 API 호출
     const response = await serverApiClient.get('/owner/no-show-posts', {
       params: {
         page: page,
         size: size,
-        date: date,
+        date: formattedDate,
       },
     });
 
