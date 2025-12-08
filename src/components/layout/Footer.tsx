af7e353ecf7642f7e823987cc6ee3a9617455a2c
Ahhhhhhyeong <dkdud081@gmail.com>
2025-12-08 13:20:43 +0900

    [design] Design QA M-001 사장님 페이지 푸터 배치 및 디자인 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className='w-full bg-white py-16 border-t border-border-wrapper'>
+    <footer className='w-full bg-white border-t border-border-wrapper'>
       {/* ✅ 전체는 중앙 정렬 */}
-      <div className='w-full px-16 max-[400px]:px-16 lg:px-20 py-40'>
+      <div className='w-full px-16 max-[400px]:px-16 lg:px-[40px] pt-40 pb-20'>
         {/* ✅ 내부 컨테이너 */}
-        <div className='w-full mx-auto flex flex-col gap-5'>
+        <div className='max-w-[1280px] mx-auto flex flex-col gap-5'>
           {/* 상단: 로고 + 링크 + 고객센터 */}
           <div className='flex flex-row justify-between items-start max-[400px]:flex-col max-[400px]:items-left max-[400px]:gap-20'>
             {/* 왼쪽 */}
-            <div className='flex flex-col max-[400px]:flex-row items-start max-[400px]:items-center gap-5'>
-              <div className=' title5 max-[400px]:title8'>단디온나</div>
+            <div className='flex flex-col min-[400px]:flex-row items-start min-[400px]:items-center gap-[20px]'>
+              <div className=' title7 max-[400px]:title5'>단디온나</div>
 
-              <div className='flex flex-wrap items-center gap-4 max-[400px]:gap-3 text-xs text-neutral-900'>
-                <span className='caption4 cursor-pointer hover:underline'>브랜드 소개</span>
-                <span className='caption4 cursor-pointer hover:underline'>이용약관</span>
-                <span className='caption4 font-bold max-[400px]:font-medium cursor-pointer hover:underline'>
+              <div className='flex flex-wrap items-center gap-[20px] max-[400px]:gap-3 text-xs text-neutral-900'>
+                <span className='caption3 cursor-pointer hover:underline hover:font-bold transition-all'>브랜드 소개</span>
+                <span className='caption3 cursor-pointer hover:underline hover:font-bold transition-all'>이용약관</span>
+                <span className='caption3 cursor-pointer hover:underline hover:font-bold transition-all'>
                   개인정보처리방침
                 </span>
               </div>
             </div>
 
             {/* 오른쪽 */}
-            <div className='flex flex-col max-[400px]:flex-row max-[400px]:items-center max-[400px]:gap-2 text-right max-[400px]:text-left'>
-              <span className='text-neutral-900 text-base font-medium max-[400px]:body1'>고객센터</span>
-              <span className='text-primary text-xl font-bold max-[400px]:caption8'>1500-0000</span>
+            <div className='flex flex-col min-[400px]:flex-row min-[400px]:items-center min-[400px]:gap-2 text-right min-[400px]:text-left gap-[10px]'>
+              <span className='text-neutral-900 text-base font-medium min-[400px]:body1'>고객센터</span>
+              <span className='text-primary text-xl font-bold min-[400px]:caption8'>1500-0000</span>
             </div>
           </div>
 
           {/* 하단: 주소 및 카피라이트 */}
-          <div className='flex flex-col gap-1.5 caption4 text-neutral-900 mt-2'>
+          <div className='flex flex-col gap-1.5 caption3 text-neutral-900 mt-2'>
             <p>경기 성남시 분당구 구름스퀘어 (주)와이낫컴퍼니</p>
             <p>Copyright © 2025 Ynot. All Rights Reserved.</p>
           </div>
