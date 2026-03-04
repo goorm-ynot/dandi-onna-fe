3abc2e5406c0d8af427cabbda988a719fc41ee36
Ahhhhhhyeong <dkdud081@gmail.com>
2026-03-04 13:41:33 +0900

    [fix] 프리셋 처음 접속 시 렌더링 이슈 해결

--- src/app/seller/mypage/preset/page.tsx
+++ src/app/seller/mypage/preset/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+/**
+ * TODO: 
+ * 프리셋 관리 페이지 구현 (1/3) [v]
+ * 프리셋 저장 시, 저장 완료 다이얼로그 구현 (2/3) [v]
+ * 프리셋 불러올 때, 기본 이외의 프리셋이 있는경우 화면 어떻게 보여줄지 고민 필요 (3/3) []
+ * 프리셋 호출 할 때 로딩 상태 보여주기 [] 
+ *    - 사실 첫 호출이후 계속 로딩이 뜸 -> 캐싱 필요할듯?
+ */
+
 import DashBoardLayout from "@/components/layout/DashboardLayout";
 import {
   BasePolicySection,
--- src/constants/sellerNavConstant.ts
+++ src/constants/sellerNavConstant.ts
@@ -33,11 +33,11 @@ export const MENU_ITEMS: MenuItem[] = [
     label: '상품/재고',
     path: '/no-show',
     children: [
-      { id: 'product-management', label: '상품 관리' },
-      { id: 'stock-management', label: '재고 관리' },
+      // { id: 'product-management', label: '상품 관리' },
+      // { id: 'stock-management', label: '재고 관리' },
       { id: 'menu-management', label: '메뉴 관리' },
       { id: 'noshow-menu-management', label: '노쇼 메뉴 관리', path: '/no-show' },
-      { id: 'noshow-menu-create', label: '노쇼 메뉴 등록' },
+      // { id: 'noshow-menu-create', label: '노쇼 메뉴 등록' },
     ],
   },
   {
